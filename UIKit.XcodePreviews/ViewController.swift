76c2e9f40980028ad81b310366faa09860fb348d
Leandro Romano <leandroromano.dev@gmail.com>
2019-11-14 20:30:14 -0300

    Setup project's base.

---
+++ UIKit.XcodePreviews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIKit.XcodePreviews
+//
+//  Created by Leandro Romano on 14/11/19.
+//  Copyright © 2019 Leandro Romano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+
+
+}
+
